b6c0d39eb46567398998c9dc71c9d0da215b2260
Andrew Warner <wwarner.andrew@gmail.com>
2014-04-03 08:47:21 -0400

    Fix example spec

--- spec/views/users/show_spec.rb
+++ spec/views/users/show_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe Users::Show do
   it 'should be easy to test!' do
     user = double(:user, email: 'foo@bar.com')
-    users_show = Users::Show.new(user)
+    users_show = Users::Show.new(double, user)
 
     users_show.email.should == user.email
   end
