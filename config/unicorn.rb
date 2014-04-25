a22bc10a12bdf1887ccd7d9ee5efb589d8f005aa
Andrew Warner <andrew@rapgenius.com>
2014-04-25 17:56:50 -0400

    Forgot to touch initialized file

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -3,6 +3,8 @@ timeout 15
 preload_app true
 
 before_fork do |server, worker|
+  FileUtils.touch('/tmp/app-initialized')
+
   Signal.trap 'TERM' do
     puts 'Unicorn master intercepting TERM and sending myself QUIT instead'
     Process.kill 'QUIT', Process.pid
